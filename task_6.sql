f72ca2455328de805dbcc52fe0c39902fe723964
john <jmnjogo@gmail.com>
2024-10-20 23:00:07 +0300

    task 7

--- task_6.sql
+++ task_6.sql
@@ -1,22 +1,22 @@
 USE alx_book_store;
 
 INSERT INTO customer (
-    SET customer_id = 2,
-        customer_name = Blessing Malik,
-        email = bmalik@sandtech.com,
-        address = 124 Happiness  Ave.
+    customer_id = 2,
+    customer_name = Blessing Malik,
+    email = bmalik@sandtech.com,
+    address = 124 Happiness  Ave.
 );
 
 INSERT INTO customer (
-    SET customer_id = 3,
-        customer_name = Obed Ehoneah,
-        email = eobed@sandtech.com,
-        address = 125 Happiness Ave.
+    customer_id = 3,
+    customer_name = Obed Ehoneah,
+    email = eobed@sandtech.com,
+    address = 125 Happiness Ave.
 );
 
 INSERT INTO customer (
-    SET customer_id = 4,
-        customer_name = Nehemial Kamolu,
-        email = nkamolu@sandtech.com,
-        address = 126 Happiness Ave.
+    customer_id = 4,
+    customer_name = Nehemial Kamolu,
+    email = nkamolu@sandtech.com,
+    address = 126 Happiness Ave.
 );
