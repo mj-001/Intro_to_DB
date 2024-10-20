bd409b0e897d07635b2300ad47560ca35f7f9f09
john <jmnjogo@gmail.com>
2024-10-20 21:57:56 +0300

    task 2

--- task_2.sql
+++ task_2.sql
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 
 
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
