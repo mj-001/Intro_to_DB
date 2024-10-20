d44a7e6d7a772363d55304562058ebfd2f22f294
john <jmnjogo@gmail.com>
2024-10-20 20:23:59 +0300

    task 0

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -39,6 +39,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id),
 );
