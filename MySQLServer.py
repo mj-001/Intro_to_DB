12d804ad5940ece5de013958b59ef5eefdac8cac
john <jmnjogo@gmail.com>
2024-10-20 20:49:36 +0300

    task 1

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,50 +1,33 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-def connect_to_mysql_server():
+
+def create_database():
     try:
         connection = mysql.connector.connect(
             host="localhost",
             user="john_njogo",
             password="ALX_MYSQL"
         )
-        print("Connection to MySQL server successful.")
-        return connection
+
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        connection.commit()
+
+        print("Database 'alx_book_store' created successfully!")
+
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Error: Access denied. Check your username or password.")
         else:
             print(f"Error: {err}")
-        return None
 
-
-def create_database(connection):
-    try:
-        cursor = connection.cursor()
-        db_name = "alx_book_store"
-        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
-        connection.commit()
-        print(f"Database '{db_name}' created successfully!")
-    except mysql.connector.Error as err:
-        print(f"Error: Could not create database. {err}")
     finally:
-        cursor.close()
-
-
-def close_connection(connection):
-    if connection.is_connected():
-        connection.close()
-        print("MySQL connection is closed.")
-
-
-
-def main():
-    connection = connect_to_mysql_server()
-    if connection:
-        create_database(connection)
-        close_connection(connection)
-
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed.")
 
 
 if __name__ == "__main__":
-    main()
+    create_database()
