5088c7f733f6d142d24e1107546428cf4b7f6e3d
john <jmnjogo@gmail.com>
2024-10-20 22:49:15 +0300

    task 7

--- task_6.sql
+++ task_6.sql
@@ -5,12 +5,16 @@ INSERT INTO customer (
     customer_name = `Blessing Malik
     email = bmalik@sandtech.com
     address = 124 Happiness  Ave.,
+);
 
+INSERT INTO customer (
      customer_id = 3
      customer_name = Obed Ehoneah
      email = eobed@sandtech.com
      address = 125 Happiness  Ave.
+);
 
+INSERT INTO customer (
       customer_id = 4
       customer_name = Nehemial Kamolu
       email = nkamolu@sandtech.com
